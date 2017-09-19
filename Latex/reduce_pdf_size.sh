970003126c155c12594c7fa03b37b428492c7dce
Robert Krug <robert.krug@oru.se>
2017-09-19 16:41:06 +0200

    initial commit

---
+++ Latex/reduce_pdf_size.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default \
+    -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages \
+    -dCompressFonts=true -r600 -sOutputFile=cv_reduced.pdf cv.pdf
---
+++ reduce_pdf_size.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default \
+    -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages \
+    -dCompressFonts=true -r600 -sOutputFile=Diploma_degree_reduced.pdf Diploma_degree.pdf
